ceb675b20462e8fa8761bc0ef0a3c31b3128438c
Joey Dreijer <info@d3vzer0.com>
2019-02-18 01:27:28 +0100

    Changes to startup script to configure iptables and routing

--- teamservers/haproxy/set_iptables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-iptables -t mangle -N DIVERT
-iptables -t mangle -A PREROUTING -p tcp -m socket -j DIVERT
-iptables -t mangle -A DIVERT -j MARK --set-mark 1
-iptables -t mangle -A DIVERT -j ACCEPT
--- teamservers/haproxy/set_routes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ip rule add fwmark 1 lookup 100
-ip route add local 0.0.0.0/0 dev lo table 100
