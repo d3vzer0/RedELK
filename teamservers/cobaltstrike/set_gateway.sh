e37070c70e53167e3d56336a6cebc900d7797de8
Joey Dreijer <info@d3vzer0.com>
2019-02-25 03:35:58 +0100

    Fixed executable permissions and added 8s sleep to idle for network

--- teamservers/cobaltstrike/set_gateway.sh
+++ teamservers/cobaltstrike/set_gateway.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+# Static sleep to wait for Haproxy route to start up
+sleep 8
+
+# Set gateway to Haproxy
 GW=`dig +short haproxy`
 /sbin/ip route del default
 /sbin/ip route add default via $GW dev eth0
